332d5d0fe7d1169d5efd3cace280d25900d5eb96
David Danforth <ddanforth@mrss.com>
2017-07-28 11:27:21 -0400

    add default config for db and new functionality

--- config.php
+++ config.php
@@ -1,5 +1,27 @@
 <?php 
+date_default_timezone_set('America/New_York');
+$dbopts = parse_url(getenv('DATABASE_URL'));
 
-$dsn = $dbopts['scheme']."://host=".$dbopts['host'].";port=".$dbopts['port'].";dbname=".ltrim($dbopts["path"],'/').";user=".$dbopts['user'].";password=".$dbopts['pass'];
+require('vendor/autoload.php');
+use Silex\Application;
+
+
+
+
+
+$app = new Application();
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+   array(
+    'pdo.server' => array(
+       'driver'   => 'pgsql',
+       'user' => $dbopts["user"],
+       'password' => $dbopts["pass"],
+       'host' => $dbopts["host"],
+       'port' => $dbopts["port"],
+       'dbname' => ltrim($dbopts["path"],'/')
+       )
+   )
+);
 
 ?>
