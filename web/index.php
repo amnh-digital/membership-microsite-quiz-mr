983552e3efa5699dc83e219ea7dec91e30e74a24
David Danforth <ddanforth@mrss.com>
2017-08-01 16:17:57 -0400

    add analytics

--- web/index.php
+++ web/index.php
@@ -31,14 +31,29 @@ require('../config.php');
 	<link rel="Copyright" href="/about-the-museum/copyright" title="Copyright"/>
 	<link rel="Author" href="/about-the-museum" title="Author"/>
 
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WZCFGBM');</script>
+	<!-- End Google Tag Manager -->
+
 	<link rel="stylesheet" href="/dist/css/main.css" type="text/css">
 	<script type="text/javascript" src="/dist/js/main.js" charset="utf-8"></script>
+	<script src="https://cdn.optimizely.com/js/5768981649.js"></script>
 
 	<script src="https://use.typekit.net/oqo1hqs.js"></script>
 	<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
 </head>
 <body>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZCFGBM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <div id="screen"></div>
 
 
