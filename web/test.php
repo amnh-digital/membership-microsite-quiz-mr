c09043774104b46ed2e620cbd9b6ac7af7085df1
David Danforth <ddanforth@mrss.com>
2017-08-01 13:05:18 -0400

    add a test page for mobile qa

--- web/test.php
+++ web/test.php
@@ -94,12 +94,22 @@ require('../config.php');
 
 
 #fooWrap {
-	position: absolute;
 	bottom:0px;
-	width: 700px;
-	max-width: 700px;
+	width: 100%;
+	max-width: 100%;
 	height: 200px;
 	overflow:hidden;
+	position: relative;
+}
+#inner {
+	width: 100%;
+	max-width: 100%;
+	overflow: auto;
+	position: absolute;
+	right: -15px;
+	 top: 0;
+    bottom: -15px;
+    left: 0;
 }
 #foo {
 	width: 2000px;
@@ -108,6 +118,21 @@ require('../config.php');
 
 }
 </style>
+
+
+
+
+<div id="out">
+<div id="fooWrap">
+	<div id="inner" class="dragscroll">
+		<div id="foo">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eros tellus, lobortis eu dictum maximus, tincidunt mattis libero. Vestibulum interdum gravida metus ornare condimentum. Nunc pulvinar nibh vitae enim tincidunt, sed viverra sapien tempus. Vestibulum luctus sed dui et convallis. Proin eros tortor, dignissim a tellus at, vestibulum posuere nulla. Donec eu turpis ex. Pellentesque a ipsum sagittis, varius sapien quis, facilisis magna. Curabitur aliquet enim sed ex bibendum feugiat.<br /><br />Curabitur varius viverra lacus non bibendum. Cras sed orci et sapien elementum sodales. Mauris non lacus eu enim ullamcorper sodales vehicula eu purus. Morbi fermentum neque consectetur odio pellentesque finibus. Donec venenatis faucibus orci eu auctor. Quisque scelerisque blandit quam eu pulvinar. Etiam convallis quam id justo consequat feugiat. Ut pulvinar aliquam auctor. Maecenas blandit orci nec ligula viverra, non mattis lectus vestibulum. Suspendisse pellentesque dignissim erat in accumsan. In tempus lacus quis tortor faucibus volutpat.<br /><br />Quisque quis pretium odio, vel lacinia libero. Cras feugiat gravida risus, dignissim varius massa. Proin molestie volutpat velit, quis venenatis metus scelerisque luctus. Etiam tincidunt pharetra est, id placerat leo. Pellentesque ultricies sed lacus quis bibendum. Praesent eget odio non urna euismod vehicula eu ac arcu. Nullam in dapibus tellus. Cras at nisi metus. In at diam gravida, dictum eros maximus, vestibulum risus. Donec eu condimentum nulla. Donec ut risus non nibh iaculis dapibus et et velit. Sed commodo rutrum sapien vel fringilla. Quisque pharetra lorem sit amet nunc viverra sagittis. In scelerisque ex eu eros malesuada tincidunt. Aenean sed lobortis dui. Sed fermentum massa vitae metus vehicula fermentum.
+		</div>
+	</div>
+</div>
+</div>
+
+
 
 <!--
 <div id="parent" class="dragscroll">
@@ -120,35 +145,20 @@ require('../config.php');
 </div>
 </div></div>
 </div></div>
--->
+
 
 
 <div class="scrollable_y intence dragscroll river_frame" id="river_scroll" style="">
-  <div style="position: relative; width: 100%; height: 100%;">
+  <div style="position: relative; width: 100%; height: 100%;overflow:scroll;">
     <div style="position: absolute; width: 100%; height: 100%; isolation: isolate;">
       <div style="position: absolute; overflow: scroll; z-index: 0;">
-        <div style="position:relative;float: left; width: 100%; height: 230px;">
-          <div style="outline: rgb(70, 74, 77) none 0px; padding: 0px;">
             <div class="river_wrap">
             	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eros tellus, lobortis eu dictum maximus, tincidunt mattis libero. Vestibulum interdum gravida metus ornare condimentum. Nunc pulvinar nibh vitae enim tincidunt, sed viverra sapien tempus. Vestibulum luctus sed dui et convallis. Proin eros tortor, dignissim a tellus at, vestibulum posuere nulla. Donec eu turpis ex. Pellentesque a ipsum sagittis, varius sapien quis, facilisis magna. Curabitur aliquet enim sed ex bibendum feugiat.<br /><br />Curabitur varius viverra lacus non bibendum. Cras sed orci et sapien elementum sodales. Mauris non lacus eu enim ullamcorper sodales vehicula eu purus. Morbi fermentum neque consectetur odio pellentesque finibus. Donec venenatis faucibus orci eu auctor. Quisque scelerisque blandit quam eu pulvinar. Etiam convallis quam id justo consequat feugiat. Ut pulvinar aliquam auctor. Maecenas blandit orci nec ligula viverra, non mattis lectus vestibulum. Suspendisse pellentesque dignissim erat in accumsan. In tempus lacus quis tortor faucibus volutpat.<br /><br />Quisque quis pretium odio, vel lacinia libero. Cras feugiat gravida risus, dignissim varius massa. Proin molestie volutpat velit, quis venenatis metus scelerisque luctus. Etiam tincidunt pharetra est, id placerat leo. Pellentesque ultricies sed lacus quis bibendum. Praesent eget odio non urna euismod vehicula eu ac arcu. Nullam in dapibus tellus. Cras at nisi metus. In at diam gravida, dictum eros maximus, vestibulum risus. Donec eu condimentum nulla. Donec ut risus non nibh iaculis dapibus et et velit. Sed commodo rutrum sapien vel fringilla. Quisque pharetra lorem sit amet nunc viverra sagittis. In scelerisque ex eu eros malesuada tincidunt. Aenean sed lobortis dui. Sed fermentum massa vitae metus vehicula fermentum.
             </div>
-          </div>
-        </div>
       </div>
     </div>
   </div>
-</div>
-
-
-
-<div id="fooWrap" class="dragscroll">
-	<div id="foo">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eros tellus, lobortis eu dictum maximus, tincidunt mattis libero. Vestibulum interdum gravida metus ornare condimentum. Nunc pulvinar nibh vitae enim tincidunt, sed viverra sapien tempus. Vestibulum luctus sed dui et convallis. Proin eros tortor, dignissim a tellus at, vestibulum posuere nulla. Donec eu turpis ex. Pellentesque a ipsum sagittis, varius sapien quis, facilisis magna. Curabitur aliquet enim sed ex bibendum feugiat.<br /><br />Curabitur varius viverra lacus non bibendum. Cras sed orci et sapien elementum sodales. Mauris non lacus eu enim ullamcorper sodales vehicula eu purus. Morbi fermentum neque consectetur odio pellentesque finibus. Donec venenatis faucibus orci eu auctor. Quisque scelerisque blandit quam eu pulvinar. Etiam convallis quam id justo consequat feugiat. Ut pulvinar aliquam auctor. Maecenas blandit orci nec ligula viverra, non mattis lectus vestibulum. Suspendisse pellentesque dignissim erat in accumsan. In tempus lacus quis tortor faucibus volutpat.<br /><br />Quisque quis pretium odio, vel lacinia libero. Cras feugiat gravida risus, dignissim varius massa. Proin molestie volutpat velit, quis venenatis metus scelerisque luctus. Etiam tincidunt pharetra est, id placerat leo. Pellentesque ultricies sed lacus quis bibendum. Praesent eget odio non urna euismod vehicula eu ac arcu. Nullam in dapibus tellus. Cras at nisi metus. In at diam gravida, dictum eros maximus, vestibulum risus. Donec eu condimentum nulla. Donec ut risus non nibh iaculis dapibus et et velit. Sed commodo rutrum sapien vel fringilla. Quisque pharetra lorem sit amet nunc viverra sagittis. In scelerisque ex eu eros malesuada tincidunt. Aenean sed lobortis dui. Sed fermentum massa vitae metus vehicula fermentum.
-	</div>
-</div>
-
-
-
+</div>-->
 
 
 <script>
